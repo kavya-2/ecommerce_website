188ebe6bc380e301902ac1980fdf25569332a9ca
kavya-2 <kavya.sivapal@gmail.com>
2024-08-09 17:59:35 +0530

    feat: Add Product model

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,56 @@
+class Admin::ProductsController < ApplicationController
+	before_action :require_admin
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product), notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: 'Product was successfully deleted.'
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, {images: []}, {colors: []}, {sizes: []}, :category_id)
+  end
+
+  def require_admin
+    unless current_user&.admin?
+      redirect_to root_path, alert: "Access denied."
+    end
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,15 @@
 class Product < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :category
+  has_many_attached :images
   serialize :images, Array
   serialize :variants, Array
+  serialize :colors, Array
+  serialize :sizes, Array
+
+  def as_indexed_json(options = {})
+    as_json(only: [:title, :description, :price, :colors, :sizes, :category_id])
+  end
 end
